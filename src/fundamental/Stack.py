d54da0e753f44be477828d308b8104e8c1b1c9bb
taojiaa <jiatao1993@gmail.com>
2019-09-18 17:59:52 +0800

    wip

--- src/fundamental/Stack.py
+++ src/fundamental/Stack.py
@@ -1,13 +1,15 @@
 class Stack:
 
-    def __init__(self):
-        self._stack = []
+    def __init__(self, capacity=2):
+        self._stack = [None] * capacity
         self._size = 0
 
     def is_empty(self):
         return self._size == 0
 
     def push(self, val):
+        if self._size > len(self._stack) // 2:
+            self._resize(len(self._stack) * 2)
         self._stack.append(val)
         self._size = self._size + 1
 
@@ -20,10 +22,15 @@ class Stack:
     def size(self):
         return self._size
 
+    def _resize(self, n):
+        temp = [None] * n
+        for i in range(len(self._stack)):
+            temp[i] = self._stack[i]
+        self._stack = temp
+
     def __iter__(self):
         while True:
             if self.is_empty():
                 return
             else:
                 yield self.pop()
-
