b82d714e956b5b0d92f35a8de62f0d31978db3d7
taojiaa <jiatao1993@gmail.com>
2019-09-20 21:51:13 +0800

    add IndexMinPQ

--- src/sort/PQ.py
+++ src/sort/PQ.py
@@ -15,7 +15,7 @@ class PQ:
     def insert(self, val):
         if self._size >= len(self.pq) - 1:
             self._resize(len(self.pq) * 2)
-        self._size = self._size + 1
+        self._size += 1
         self.pq[self._size] = val
         self._swim(self._size)
 
@@ -42,7 +42,7 @@ class MaxPQ(PQ):
         max_val = self.pq[1]
         self.pq[1], self.pq[self._size] = self.pq[self._size], self.pq[1]
         self.pq[self._size + 1] = None
-        self._size = self._size - 1
+        self._size -= 1
         self._sink(self.pq, 1, self.size())
         return max_val
 
@@ -75,7 +75,7 @@ class MinPQ(PQ):
         min_val = self.pq[1]
         self.pq[1], self.pq[self._size] = self.pq[self._size], self.pq[1]
         self.pq[self._size + 1] = None
-        self._size = self._size - 1
+        self._size -= 1
         self._sink(self.pq, 1, self.size())
         return min_val
 
@@ -98,6 +98,90 @@ class MinPQ(PQ):
             else:
                 break
             k = j
+
+
+class IndexMinPQ(MinPQ):
+
+    def __init__(self, max_n):
+        self._maxn = max_n
+        self._size = 0
+        # _ind is a priority queue that we always maintains.
+        self._ind = [None] * (self._maxn + 1)
+        # _map stores the mapping from index k to the position i in _ind.
+        self._map = [None] * (self._maxn + 1)
+        # _val stores the value, putting it in the "index" position it links to.
+        self._val = [None] * (self._maxn + 1)
+
+    def insert(self, k, val):
+        self._size += 1
+        self._ind[self._size] = k
+        self._map[k] = self._size
+        self._val[k] = val
+        self._swim(self._size)
+
+    def change(self, k, val):
+        self._val[k] = val
+        self._swim(self._map[k])
+        self._sink(self._map[k])
+
+    def _swim(self, k):
+        while k > 1:
+            j = k // 2
+            ind_1, ind_2 = self._ind[k], self._ind[j]
+            if self._val[ind_1] < self._val[ind_2]:
+                self._exch(k, j)
+            else:
+                break
+            k = j
+
+    def _sink(self, k):
+        while (k * 2) <= self._size:
+            j = k * 2
+            ind_1, ind_2 = self._ind[j], self._ind[j + 1]
+            if j < self._size and self._val[ind_1] > self._val[ind_2]:
+                j = j + 1
+            ind_1, ind_2 = self._ind[k], self._ind[j]
+            if self._val[ind_1] > self._val[ind_2]:
+                self._exch(k, j)
+            else:
+                break
+            k = j
+
+    def _exch(self, i, j):
+        self._ind[i], self._ind[j] = self._ind[j], self._ind[i]
+        # _ind[i] stores the new index at the position i in _ind,
+        # so we need to map the new index to position i.
+        self._map[self._ind[i]] = i
+        self._map[self._ind[j]] = j
+
+    def contains(self, k):
+        if self._val[k] is not None:
+            return True
+        return False
+
+    def delete(self, k):
+        pos = self._map[k]
+        self._exch(pos, self._size)
+        self._size -= 1
+        self._swim(pos)
+        self._sink(pos)
+        self._val[k] = None
+        self._map[k] = None
+    
+    def del_min(self):
+        ind = self._ind[1]
+        self._exch(1, self._size)
+        self._size -= 1
+        self._sink(1)
+        self._val[ind] = None
+        self._map[ind] = None
+        return ind
+
+    def min(self):
+        return self._val[self._ind[1]]
+
+    def min_index(self):
+        return self._ind[1]
 
 
 class HeapSort(Sort, MaxPQ):
--- tests/sort/test_pq.py
+++ tests/sort/test_pq.py
@@ -1,15 +1,23 @@
 import string
 from random import shuffle
 
-from src.sort.PQ import MaxPQ, MinPQ
+from src.sort.PQ import MaxPQ, MinPQ, IndexMinPQ
 
 
 def initiate(test_class):
     array = list(string.ascii_lowercase) * 2
     shuffle(array)
-    pq = test_class(20)
+    pq = test_class()
     for i in array:
         pq.insert(i)
+    return pq
+
+
+def initiate_index_val(test_class):
+    dic = {25 - i: v for i, v in enumerate(string.ascii_lowercase)}
+    pq = test_class(len(dic))
+    for key, val in dic.items():
+        pq.insert(key, val)
     return pq
 
 
@@ -31,3 +39,31 @@ class TestMinPQ:
         assert pq.size() == 26 * 2 - 1
         assert pq.del_min() == 'a'
         assert pq.del_min() == 'b'
+
+
+class TestIndexMinPQ:
+
+    def test_insert(self):
+        pq = initiate_index_val(IndexMinPQ)
+        assert pq.size() == 26
+        assert pq.contains(25)
+        assert not pq.contains(26)
+        assert not pq.is_empty()
+
+    def test_delete(self):
+        pq = initiate_index_val(IndexMinPQ)
+        for i in range(26):
+            pq.delete(i)
+        assert pq.is_empty()
+
+    def test_delete_min(self):
+        pq = initiate_index_val(IndexMinPQ)
+        m = pq.del_min()
+        assert m == 25
+        m = pq.del_min()
+        assert m == 24
+
+    def test_change(self):
+        pq = initiate_index_val(IndexMinPQ)
+        pq.change(25, 'bb')
+        assert pq.min() == 'b'
