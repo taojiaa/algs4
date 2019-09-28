bc2b45ba4d2a9c6b2c5b1ded23b247680cd3f4f9
taojiaa <jiatao1993@gmail.com>
2019-09-28 14:02:01 +0800

    fix bugs

--- src/string/base.py
+++ src/string/base.py
@@ -1,79 +1,79 @@
-from abc import ABC, abstractclassmethod
+from abc import ABC, abstractmethod
 
 
 class StringST(ABC):
 
-    @abstractclassmethod
+    @abstractmethod
     def __init__(self):
         '''
         create a symbol table.
         '''
         pass
 
-    @abstractclassmethod
+    @abstractmethod
     def get(self, key):
         '''
         value paired with key (null if key is absent).
         '''
         pass
 
-    @abstractclassmethod
+    @abstractmethod
     def put(self, key, val):
         '''
         put key-value pair into the table (remove key if value is null)
         '''
         pass
 
-    @abstractclassmethod
+    @abstractmethod
     def delete(self, key):
         '''
         remove key (and its value).
         '''
         pass
 
-    @abstractclassmethod
+    @abstractmethod
     def contains(self, key):
         '''
         is there a value paired with key?
         '''
         pass
 
-    @abstractclassmethod
+    @abstractmethod
     def is_empty(self):
         '''
         is the table empty?
         '''
         pass
 
-    @abstractclassmethod
+    @abstractmethod
     def longest_prefix_of(self, s):
         '''
         the longest key that is a prefix of s.
         '''
         pass
 
-    @abstractclassmethod
+    @abstractmethod
     def keys_with_prefix(self, s):
         '''
         all the keys having s as a prefix.
         '''
         pass
 
-    @abstractclassmethod
+    @abstractmethod
     def keys_that_match(self, s):
         '''
         all the keys that match s (where . matches any character).
         '''
         pass
 
-    @abstractclassmethod
+    @abstractmethod
     def size(self):
         '''
         number of key-value pairs.
         '''
         pass
 
-    @abstractclassmethod
+    @abstractmethod
     def keys(self):
         '''
         all the keys in the table.
